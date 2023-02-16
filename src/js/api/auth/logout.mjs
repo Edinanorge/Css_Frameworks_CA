f2ef1415d0702f44289485448a76f57833355474
Edinanorge <52853951+Edinanorge@users.noreply.github.com>
2023-02-16 23:14:06 +0100

    storage uppdate

---
+++ src/js/api/auth/logout.mjs
@@ -0,0 +1,6 @@
+import * as storage from "../../storage/index.mjs";
+
+export function logout() {
+  storage.remove("token");
+  storage.remove("user");
+}
--- src/js/storage/index.mjs
+++ src/js/storage/index.mjs
@@ -1,5 +1,3 @@
-import { load } from "./load.mjs";
-import { remove } from "./remove.mjs";
-import { save } from "./save.mjs";
-
-export default { load, remove, save };
+export * from "./load.mjs";
+export * from "./remove.mjs";
+export * from "./save.mjs";
