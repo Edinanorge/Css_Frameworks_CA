2150b06b53487cb9997ec7f902e194b4b33f9465
Edinanorge <52853951+Edinanorge@users.noreply.github.com>
2023-02-17 23:45:34 +0100

    postMethodes added

--- src/js/api/posts/read.mjs
+++ src/js/api/posts/read.mjs
@@ -1,2 +1,31 @@
-export async function getPosts() {}
-export async function getPost(id) {}
+import { url } from "../constants.mjs";
+import { fetchWhitToken } from "./headers.mjs";
+
+export async function getPosts() {
+  const getPostsUrl = `${url}/posts`;
+
+  const response = await fetchWhitToken(getPostsUrl);
+
+  if (response.ok) {
+    const post = await response.json();
+    return post;
+  } else {
+    throw new Error(response.statusText);
+  }
+}
+
+export async function getPost(id) {
+  if (!id) {
+    throw new Error("Get requires a postId");
+  }
+
+  const getPostsUrl = `${url}/posts/${id}`;
+
+  const response = await fetchWhitToken(getPostsUrl);
+
+  if (response.ok) {
+    return await response.json();
+  } else {
+    throw new Error(response.statusText);
+  }
+}
