f6b47fd97226cea7df5f8d4f137d37415e61f8d2
Edinanorge <52853951+Edinanorge@users.noreply.github.com>
2023-02-23 18:15:08 +0100

    Profile Methodes created

---
+++ src/js/api/profiles/getPosts.mjs
@@ -0,0 +1,18 @@
+import { url } from "../constants.mjs";
+import { fetchWhitToken } from "../posts/headers.mjs";
+
+export async function getProfilePosts(name) {
+  if (!name) {
+    throw new Error("Get requires a user name");
+  }
+
+  const getProfilePostsUrl = `${url}/profiles/${name}/posts`;
+
+  const response = await fetchWhitToken(getProfilePostsUrl);
+
+  if (response.ok) {
+    return await response.json();
+  } else {
+    throw new Error(response.statusText);
+  }
+}
