e63f58e607fdf999163c2daf96c479c89c03acab
Edinanorge <52853951+Edinanorge@users.noreply.github.com>
2023-02-27 13:19:33 +0100

    success and error messages added

--- src/js/helpers/displayMessage.mjs
+++ src/js/helpers/displayMessage.mjs
@@ -1,5 +1,5 @@
-export function displayMessage(message, style) {
-  const feedback = document.querySelector("#message");
+export function displayMessage(container, message, style) {
+  const feedback = document.getElementById(container);
   feedback.innerText = message;
   feedback.classList.add(style);
 }
